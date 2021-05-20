91f1db84ecfee75b37d363d8f9accde6b60171c0
maxfield <maxfielddsmith@gmail.com>
2021-05-20 15:46:03 -0700

    Completed Fixed nav bar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import About from "./About";
+import NavBar from "./NavBar";
 
 export default function App() {
   return (
     <div className="page-container">
       <Router>
         <div>
+          <NavBar />
+
           <Switch>
             <Route exact path="/" component={About} />
             <Route path="/resume">
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="navContainer">
+        <div className="row rowNav">
+          <div className="home-button">
+            <Link to="/">
+              <img
+                width="172px"
+                height="60px"
+                src="maxlogo.png"
+                alt="My Logo logo"
+              />
+            </Link>
+          </div>
+          <div className="navButtons">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">About Me</Link>
+                </li>
+                <li>
+                  <Link to="/resume">Education</Link>
+                </li>
+                <li>
+                  <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact Me</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
